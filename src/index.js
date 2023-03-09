d569fd6b18ab7605426311e2122418019ab85312
KseniiaBryzhko <kseniya.brizhko@gmail.com>
2023-03-09 16:26:33 +0200

    finished

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
 import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -13,45 +14,65 @@ searchInputEl.addEventListener(
   debounce(handleSearchCountry, DEBOUNCE_DELAY)
 );
 
-function handleSearchCountry({ target }) {
-  const searchQuery = target.value.toLowerCase().trim();
+function handleSearchCountry(event) {
+  const searchQuery = event.target.value.toLowerCase().trim();
   console.log(searchQuery);
 
+  if (!searchQuery) {
+    countryInfoEl.innerHTML = '';
+    countryListEl.innerHTML = '';
+    return;
+  }
+
   fetchCountries(searchQuery)
-    .then(createCountryCard)
-    .catch(error => console.log(error));
-
-  // if (!searchQuery) {
-  //   countryInfoEl.innerHTML = '';
-  //   return;
-  // }
-
-  // const foundCountry = countries.find(
-  //   el => el.name.toLowerCase() === searchQuery
-  // );
-
-  // if (!foundCountry) {
-  //   console.log('не знайдено');
-  //   countryInfoEl.innerHTML = '';
-  //   return;
-  // }
-
-  countryInfoEl.innerHTML = createCountryCard(foundCountry);
+    .then(result => {
+      if (result.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        return;
+      }
+      showFoundCountries(result);
+    })
+    .catch(error => {
+      Notiflix.Notify.failure('Oops, there is no country with that name.');
+    });
 }
 
-// searchInputEl.addEventListener('input', handleSearchCountry);
+function showFoundCountries(result) {
+  if (result.length >= 2 && result.length <= 10) {
+    countryInfoEl.innerHTML = '';
+    createCountryList(result);
+  }
+  if (result.length === 1) {
+    countryListEl.innerHTML = '';
+    createCountryCard(result);
+  }
+}
 
-const createCountryCard = ({
-  name,
-  capital,
-  population,
-  flags,
-  languages,
-} = {}) => {
-  const template = `<img src="${flags.svg}" alt="${name}" width="320" height="auto">
-  <p> ${name.official}</p>
-  <ul><li>Capital: <span> ${capital}</span></li>
-  <li>Population: <span> ${population}</span></li>
-  <li>Languages: <span> ${languages}</span></li></ul>`;
-  return template;
-};
+function createCountryList(result) {
+  const countryList = result
+    .map(({ name, flags }) => {
+      return `<li class="country-list__item"><img src="${flags.svg}" alt="${name}" width="32" height="16">
+  <p> ${name.official}</p></li>`;
+    })
+    .join('');
+  countryListEl.innerHTML = countryList;
+  console.log(countryList);
+  return countryList;
+}
+
+function createCountryCard(result) {
+  const countryInfo = result
+    .map(({ name, capital, population, flags, languages }) => {
+      languages = Object.values(languages);
+      return `<div class="country-content"><img src="${flags.svg}" alt="${name}" width="32" height="16">
+  <p class="country-name"> ${name.official}</p></div>
+  <ul><li class="country-content-item">Capital: <span> ${capital}</span></li>
+  <li class="country-content-item">Population: <span> ${population}</span></li>
+  <li class="country-content-item">Languages: <span> ${languages}</span></li></ul>`;
+    })
+    .join('');
+  countryInfoEl.innerHTML = countryInfo;
+  return countryInfo;
+}
